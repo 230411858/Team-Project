3197208c5894896b55842cfea288f6c3db009851
CJAH-1 <conanjahangir26@gmail.com>
2024-11-27 11:50:12 +0000

    testing for update

--- GamerHub/public/index.php
+++ GamerHub/public/index.php
@@ -15,3 +15,5 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+    //test for pulling and pushing with tayab and the group
