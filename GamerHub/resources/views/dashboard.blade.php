be463bb05a08e73b802a76525274280fa1b9b81c
Tayyab Iqbal <230410644@aston.ac.uk>
2025-03-05 10:52:13 +0000

    Manage Customers Button

--- GamerHub/resources/views/dashboard.blade.php
+++ GamerHub/resources/views/dashboard.blade.php
@@ -13,28 +13,12 @@
                     {{ __("You're logged in! ") }}<br>
 
                     @if (Illuminate\Support\Facades\Auth::user()->account_type == 'admin')
-                        <h3>List of currently registered customers:</h3>
-                        <table>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Actions</th>
-                            </tr>
-                            @foreach (\App\Models\User::where('account_type', 'customer')->get() as $user)
-                                <tr>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>
-                                        <a href="{{ route('admin.customers.edit', $user->id) }}" class="text-blue-500">Edit</a> |
-                                        <form action="{{ route('admin.customers.destroy', $user->id) }}" method="POST" style="display:inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" onclick="return confirm('Are you sure?')" class="text-red-500">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
+                        <div class="mt-6">
+                            {{-- Manage Customers Button --}}
+                            <a href="{{ route('admin.customers.index') }}" class="manage-customers-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Manage Customers
+                            </a>
+                        </div>
                     @endif
                 </div>
             </div>
